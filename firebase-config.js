8d247f9b337208951d2845c767251c0ce06c296f
contact-temp-otp-code <contact.temp.otp@gmail.com>
2026-05-07 12:52:02 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Thay thế bằng cấu hình Firebase Console của bạn
+const firebaseConfig = {
+  apiKey: "AIzaSyASwa-huFB0ml4ZysQPXSpw4AqS2e3kde4",
+  authDomain: "thue-so-nhanh.firebaseapp.com",
+  projectId: "thue-so-nhanh",
+  storageBucket: "thue-so-nhanh.firebasestorage.app",
+  messagingSenderId: "114356248756",
+  appId: "1:114356248756:web:4977f49a179f9ab95377e1",
+  measurementId: "G-QF84MMLLNZ"
+};
